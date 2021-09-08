20516e8bce2725a09add7ed26255eb9ce0d76fd6
Maurice Saliba <maurice.saliba@go.com.mt>
2021-09-08 15:31:53 +0200

    new changes

--- dataprocessing/getUniqueJiraCategories.py
+++ dataprocessing/getUniqueJiraCategories.py
@@ -1,11 +1,10 @@
 import configparser
 import logging as log
-import numpy as np
 import pandas as pd
 from utilities.getUniqueColumnValues import getUniqueColumnValues
 
 config = configparser.RawConfigParser()
-config.read('config.properties')
+config.read('../config.properties')
 
 logsfile = config.get('Logs', 'files.log.filename')
 log.basicConfig(filename=logsfile, encoding='utf-8', level=log.DEBUG)
@@ -20,29 +19,36 @@ systemsfile = config.get('DataFiles', 'files.jira.issues.systems')
 
 dfjiraissues = pd.read_csv(mergedfilesout)
 
+
 def getuniquesprints():
     sorteduniquesprints = getUniqueColumnValues(dfjiraissues, 'Sprint')
     pd.DataFrame(sorteduniquesprints).to_csv(sprintsfile)
+
 
 def getuniqueassignees():
     sorteduniqueassignees = getUniqueColumnValues(dfjiraissues, 'Assignee')
     pd.DataFrame(sorteduniqueassignees).to_csv(assigneesfile)
 
+
 def getuniquestatuses():
     sorteduniquestatuses = getUniqueColumnValues(dfjiraissues, 'Status')
     pd.DataFrame(sorteduniquestatuses).to_csv(statusesfile)
+
 
 def getuniquereporters():
     sorteduniquereporters = getUniqueColumnValues(dfjiraissues, 'Reporter')
     pd.DataFrame(sorteduniquereporters).to_csv(reportersfile)
 
+
 def getuniquetypes():
     sorteduniquetypes = getUniqueColumnValues(dfjiraissues, 'Issue Type')
     pd.DataFrame(sorteduniquetypes).to_csv(typesfile)
 
+
 def getuniquesystems():
     sorteduniquesystems = getUniqueColumnValues(dfjiraissues, 'System/Area)')
     pd.DataFrame(sorteduniquesystems).to_csv(systemsfile)
+
 
 getuniquesprints()
 getuniqueassignees()
@@ -50,8 +56,3 @@ getuniquestatuses()
 getuniquereporters()
 getuniquetypes()
 getuniquesystems()
-
-
-
-
-
